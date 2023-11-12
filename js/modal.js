fd41e190d14e11cb500a0c3b76c9019fddc436ed
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-11-12 20:43:32 +0100

    e

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,22 @@
-(() => {
+document.addEventListener("DOMContentLoaded", function() {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  console.log("openModalBtn:", refs.openModalBtn);
+  console.log("closeModalBtn:", refs.closeModalBtn);
+  console.log("modal:", refs.modal);
+
+  if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  } else {
+    console.error("Nie znaleziono co najmniej jednego elementu. Sprawdź swoje znaczniki HTML.");
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
-})();
+});
